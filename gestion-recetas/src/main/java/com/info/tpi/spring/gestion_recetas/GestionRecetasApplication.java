d1d404b6fc064edcba123b33994cfe5f6b3c7d68
juanma479 <juanmamk9@gmail.com>
2024-10-08 00:08:52 -0300

    Primer commit

---
+++ gestion-recetas/src/main/java/com/info/tpi/spring/gestion_recetas/GestionRecetasApplication.java
@@ -0,0 +1,13 @@
+package com.info.tpi.spring.gestion_recetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionRecetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionRecetasApplication.class, args);
+	}
+
+}
---
+++ gestion-recetas/src/test/java/com/info/tpi/spring/gestion_recetas/GestionRecetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.info.tpi.spring.gestion_recetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionRecetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
